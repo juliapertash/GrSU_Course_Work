0e5a57a3f143c0749f80473e58fc21c1121e2f22
Julia Petrash <juliapetrashs@gmail.com>
2020-03-25 18:29:44 +0300

    initial commit

---
+++ webapp/Server.js
@@ -0,0 +1,15 @@
+const http=require('http');
+const fs=require('fs');
+var qs=require('querystring');
+var wp='';
+var jsFile='';
+var cssFile='';
+http.createServer((req,res)=>{
+    switch(req.url){
+case'/':
+res.writeHead(200,{'Content-Type':'text/html'});
+file=fs.readFileSync('./appdata/index.html');
+res.end(file);
+break;
+    }
+}).listen(3000,()=>console.log('Server is working'));
