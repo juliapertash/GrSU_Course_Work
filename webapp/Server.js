ae1b5b4c4366e66fd6ceeadc698daaf3e26a6ba8
Julia Petrash <juliapetrashs@gmail.com>
2020-05-06 17:35:32 +0300

    убейте меня

--- webapp/Server.js
+++ webapp/Server.js
@@ -13,3 +13,34 @@ res.end(file);
 break;
     }
 }).listen(3000,()=>console.log('Server is working'));
+const sql = require('mssql')
+ 
+var config = {
+  server: 'DESKTOP-H3854OI\SQLEXPRESS',
+  database: 'BD.ALCOSHOP',
+  user: 'user',
+  password: '1111',
+  port: 1433
+};
+function load() {
+  //4.
+  var dbConn = new sql.ConnectionPool(config);
+  //5.
+  dbConn.connect().then(function () {
+      //6.
+      var request = new sql.Request(dbConn);
+      //7.
+      request.query("select * from ASSORTMENT").then(function (recordSet) {
+          console.log(recordSet);
+          dbConn.close();
+      }).catch(function (err) {
+          //8.
+          console.log(err);
+          dbConn.close();
+      });
+  }).catch(function (err) {
+      //9.
+      console.log(err);
+  });
+}
+load();
