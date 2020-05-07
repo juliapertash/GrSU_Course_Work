dae758d10a88e720838f3ebe4d48616b026c063e
Julia Petrash <juliapetrashs@gmail.com>
2020-05-07 15:30:51 +0300

    ниче не работает

--- webapp/Server.js
+++ webapp/Server.js
@@ -1,20 +1,51 @@
-const http=require('http');
-const fs=require('fs');
-var qs=require('querystring');
-var wp='';
-var jsFile='';
-var cssFile='';
-http.createServer((req,res)=>{
-    switch(req.url){
-case'/':
-res.writeHead(200,{'Content-Type':'text/html'});
-file=fs.readFileSync('./appdata/Form.html');
-res.end(file);
-break;
-case'/MainPage.html':
-res.writeHead(200,{'Content-Type':'text/html'});
-file=fs.readFileSync('./appdata/MainPage.html');
-res.end(file);
-break;
-    }
-}).listen(3000,()=>console.log('Server is working'));
+const express = require("express");
+const bodyParser = require("body-parser");
+ 
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+const sql = require("mssql/msnodesqlv8");
+
+const conn = new sql.ConnectionPool({
+  server: 'DESKTOP-H3854OI\\SQLEXPRESS',
+  database: 'BD.ALCOSHOP',
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true
+  }
+});
+
+app.set("view engine", "hbs");
+
+app.get("/", function(req, res){
+  res.render("Form.hbs");
+});
+
+app.post("/", urlencodedParser, function(req, res){
+  if(!req.body) return res.sendStatus(400);
+  res.redirect("/MainPage.hbs");
+});
+
+app.get("/MainPage.hbs", function (req, res) {
+  conn.connect().then(function () {
+    var request = new sql.Request(conn);
+    request.query("select * from ASSORTMENT").then(function (recordSet) {
+          res.render("MainPage.hbs", {
+          items: recordSet
+          
+      }).catch(function (err) {
+          
+        console.log(err);
+        
+    });
+      
+      conn.close();
+    }).catch(function (err) {
+      
+      console.log(err);
+  });
+});
+});
+
+app.listen(3000, function(){
+  console.log("Сервер ожидает подключения...");
+});
--- webapp/views/data.js
+++ webapp/views/data.js
@@ -1,30 +1,29 @@
+var express = require('express');
+var app = express();
 const sql = require("mssql/msnodesqlv8");
 const conn = new sql.ConnectionPool({
-  server: 'DESKTOP-H3854OI\SQLEXPRESS',
+  server: 'DESKTOP-H3854OI\\SQLEXPRESS',
   database: 'BD.ALCOSHOP',
   driver: "msnodesqlv8",
   options: {
     trustedConnection: true
   }
 });
-function load() {
-  //4.
-   //5.
+
   conn.connect().then(function () {
-      //6.
+    app.get('/MainPage.html', function (req, res) {
       var request = new sql.Request(conn);
-      //7.
+      
       request.query("select * from ASSORTMENT").then(function (recordSet) {
           console.log(recordSet);
           conn.close();
       }).catch(function (err) {
-          //8.
+          
           console.log(err);
           conn.close();
       });
   }).catch(function (err) {
-      //9.
+      
       console.log(err);
   });
-}
-load();
+});
